710ccfb9aa2025c853efd61626fae7d82db73a51
Erick Tavares <129068164+ericktnunes@users.noreply.github.com>
2025-02-26 14:38:20 -0300

    Delete API-REST-de-Gerenciamento-de-Pedidos-main/API-REST-de-Gerenciamento-de-Pedidos-main directory

--- API-REST-de-Gerenciamento-de-Pedidos-main/API-REST-de-Gerenciamento-de-Pedidos-main/src/main/java/com/api_pedidos/api_pedidos/Entity/Enum/PaymentMethod.java
+++
@@ -1,8 +0,0 @@
-package com.api_pedidos.api_pedidos.Entity.Enum;
-
-public enum PaymentMethod {
-    CARTAO_CREDITO,
-    CARTAO_DEBITO,
-    PIX,
-    DINHEIRO
-}
--- API-REST-de-Gerenciamento-de-Pedidos-main/API-REST-de-Gerenciamento-de-Pedidos-main/src/main/java/com/api_pedidos/api_pedidos/Repository/OrderListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api_pedidos.api_pedidos.Repository;
-
-import com.api_pedidos.api_pedidos.Entity.OrderItems;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderListRepository extends JpaRepository<OrderItems, Long> {
-}
